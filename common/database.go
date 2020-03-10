57355cdcd35dab160ce8563a1a474fc7aa25c579
xietong <mail@xietong.me>
2020-03-10 22:57:47 +0800

    增加从文件中读取配置模块

--- common/database.go
+++ common/database.go
@@ -3,19 +3,21 @@ package common
 import (
 	"fmt"
 	"github.com/jinzhu/gorm"
+	"github.com/spf13/viper"
 	"xietong.me/ginessential/model"
 )
 
 var DB *gorm.DB
 
 func InitDB() *gorm.DB {
-	driverName := "mysql"
-	host := "localhost"
-	port := "3306"
-	database := "ginessential"
-	username := "root"
-	password := ""
-	charset := "utf8"
+	fmt.Println(viper.GetString("datasource.driverName"))
+	driverName := viper.GetString("datasource.driverName")
+	host := viper.GetString("datasource.host")
+	port := viper.GetString("datasource.port")
+	database := viper.GetString("datasource.database")
+	username := viper.GetString("datasource.username")
+	password := viper.GetString("datasource.password")
+	charset := viper.GetString("datasource.charset")
 	args := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=true",
 		username,
 		password,
--- main.go
+++ main.go
@@ -3,16 +3,31 @@ package main
 import (
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
+	"os"
 	"xietong.me/ginessential/common"
 )
 
 func main() {
+	InitConfig()
 	db := common.InitDB()
-	
 	defer db.Close()
 
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
 	panic(r.Run()) // listen and serve on 0.0.0.0:8080
-
+}
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(err)
+	}
 }
